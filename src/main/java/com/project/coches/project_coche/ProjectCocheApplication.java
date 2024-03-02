d746a4928e766e6ca3a6862ea8b0d9b1b086f60b
kevin rojas <kevin.rojas01@unicatolica.edu.co>
2024-03-01 22:24:59 -0500

    Start project

---
+++ src/main/java/com/project/coches/project_coche/ProjectCocheApplication.java
@@ -0,0 +1,13 @@
+package com.project.coches.project_coche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCocheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCocheApplication.class, args);
+	}
+
+}
